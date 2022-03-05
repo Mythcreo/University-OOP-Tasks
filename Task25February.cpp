dc1029294f254cf73a158cafaeb2f6f519d0b489
Mythcreo <99804018+Mythcreo@users.noreply.github.com>
2022-03-06 00:49:55 +0300

    Update Task25February.cpp

--- Task25February.cpp
+++ Task25February.cpp
@@ -1,24 +1,18 @@
 #include "Canvas.h"
-#include <windows.h>
-int main()
-{
-	HWND hwnd = GetConsoleWindow(); // HWND - Handle to a window
-	HDC hdc = GetDC(hwnd); // HDC - Handle to device context
-	Graphics Settings(vector <Primitive> { 
-		Primitive(new Line(500, 100, 700, 500), 2),
-		Primitive(new Rectangles(100, 180, 400, 400, 1), 4),
-		Primitive(new Ellipses(200, 200, 300, 350, 1), 3),
-		Primitive(new Rectangles(500, 500, 600, 600, 0), 4)
-	});
-	Settings.Add(Primitive(new Polygons({ {800, 400}, {1000, 300}, {800, 200} }, 3, 0), 5));
-	Settings.Add(Primitive(new Polygons({ {800, 400}, {1000, 300}, {400, 100}, {100, 400} }, 4, 0), 5));
-	Settings.Add(Primitive(new Polygons({ {400, 200}, {500, 150}, {400, 100} }, 3, 0), 5));
-	Settings.Remove(4);
+using namespace std;
 
-	Settings.Info();
-	system("pause");
-	system("cls");
-	//Settings.Clear(); // Очищение списка
-	Settings.Show(5, hwnd, hdc);
-	return 0;
+int main() {
+    Graphics StartSettings(vector <Primitive> {
+        Primitive(new Line(500, 100, 700, 500), 2),
+            Primitive(new Rectangles(100, 180, 400, 400, 1, 255, 255, 255), 4),
+            Primitive(new Ellipses(200, 200, 300, 350, 1, 0, 0, 255), 3),
+            Primitive(new Rectangles(500, 500, 600, 600, 0, 255, 255, 255), 4),
+            Primitive(new Polygons({ {800, 400}, {1000, 300}, {800, 200} }, 3, 1, 0, 0, 255), 5),
+            Primitive(new Polygons({ {800, 400}, {1000, 300}, {400, 100}, {100, 400} }, 4, 1, 0, 255, 0), 5)
+    });
+
+    Menu DesignSolution;
+    DesignSolution.Options(StartSettings);
+
+    return 0;
 }
